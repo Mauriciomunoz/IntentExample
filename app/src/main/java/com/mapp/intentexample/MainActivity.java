ee9d2c1961ec1307980bed8b279ccd38f639cfa1
Mauricio Munoz <mauricio.munoz87@gmail.com>
2020-09-13 14:49:14 -0500

    Added Implict and Explicit Intent

--- app/src/main/java/com/mapp/intentexample/MainActivity.java
+++ app/src/main/java/com/mapp/intentexample/MainActivity.java
@@ -1,14 +1,44 @@
 package com.mapp.intentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.mapp.intentexample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity{
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setMain(this);
+
     }
+
+    public void onClickExplicitIntent(){
+        Intent myIntent = new Intent(this, ImplicitIntent.class);
+        startActivity(myIntent);
+    }
+
+    public void onClickImplicitIntent(){
+        Uri webpage = Uri.parse("https://www.android.com");
+        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(webIntent);
+    }
+
+    public void onClickPendingIntent(){
+
+    }
+
+    public void onClickStickyIntent(){
+
+    }
+
 }
